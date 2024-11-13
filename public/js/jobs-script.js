d5cf5a6c7b686cd0c45adf6b323e398b614367aa
Logeshwaran123 <hlhilalogu@gmail.com>
2024-11-13 18:26:46 +0530

    Content Added

--- public/js/jobs-script.js
+++ public/js/jobs-script.js
@@ -1,150 +1,279 @@
 // Internships
 const internships = [
   {
-    headline: "Job Headline 1",
+    headline: "Digital Marketing - Intern",
     mode: "F2F",
     date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.naukri.com/job-listings-web-developer-integrator-prestashop-sahas-digi-nxt-solutions-llp-ghaziabad-0-to-4-years-061124501015?src=jobsearchDesk&sid=17311498688413925&xp=1&px=1",
-    note: "Important Note about the Role and Skillset if any",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: 5,
+    experience: "0 - 1 yrs",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "Digital marketing executive who has more knowledge in SEO , SMO , SEM , managing the position of the websites in search engines , running campaigns , Maintaining eCommerce portal and everything compared to the digital world. He / She should have the skill to work with organic searches , competitor analysis , Google analytical tool , webmaster tool , social media channels and more",
   },
   {
-    headline: "Job Headline 2",
+    headline: "Data Analyst - Intern",
     mode: "F2F",
     date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
-  },
-  {
-    headline: "Job Headline 3",
-    mode: "F2F",
-    date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
-  },
-  {
-    headline: "Job Headline 4",
-    mode: "Virtual",
-    date: "45th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
+    experience: "0 - 1 yrs",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: 5,
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "Exposure to different Software & Applications Work from Home & Flexible working hours Interns get an IAC Associate designation with Cloud Counselage Pvt. Ltd. to get continuous Industry Exposure & Experience till their graduation. This designation can be showcased in their CVs Opportunity to become Internship lead for the future internship program Opportunity for permanent employment after graduation",
   },
 ];
 
 // WFH jobs
 const wfhJobs = [
   {
-    headline: "Job Headline 1",
+    headline: "Telecalling Person",
     mode: "F2F",
     date: "12th Sep 2024",
+    experience: "0 - 1 yrs",
     location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
-  },
-  {
-    headline: "Job Headline 1",
-    mode: "F2F",
-    date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
-  },
-  {
-    headline: "Job Headline 1",
-    mode: "F2F",
-    date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
+    salary: "Not Disclosed",
+    nVacancies: 5,
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "We are seeking a motivated and results-driven Telesales Representative to join our team. The ideal candidate will be responsible for generating sales, managing customer relationships, and achieving sales targets through outbound calls. This position requires excellent communication skills, a customer-focused approach, and the ability to work in a fast-paced environment.",
   },
 ];
 
 // BPO related Jobs
-const bpoJobs = [
-  {
-    headline: "BPO related jobs",
-    mode: "F2F",
-    date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
-  },
-  {
-    headline: "Job Headline 1",
-    mode: "F2F",
-    date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
-  },
-  {
-    headline: "Job Headline 1",
-    mode: "F2F",
-    date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
-  },
-];
+// const bpoJobs = [
+//   {
+//     headline: "BPO related jobs",
+//     mode: "F2F",
+//     date: "12th Sep 2024",
+//     location: "Pondicherry",
+//     minRange: 3.25,
+//     maxRange: 3.5,
+//     link: "https://www.google.com",
+//     note: "Important Note about the Role and Skillset if any",
+//   },
+//   {
+//     headline: "Job Headline 1",
+//     mode: "F2F",
+//     date: "12th Sep 2024",
+//     location: "Pondicherry",
+//     minRange: 3.25,
+//     maxRange: 3.5,
+//     link: "https://www.google.com",
+//     note: "Important Note about the Role and Skillset if any",
+//   },
+//   {
+//     headline: "Job Headline 1",
+//     mode: "F2F",
+//     date: "12th Sep 2024",
+//     location: "Pondicherry",
+//     minRange: 3.25,
+//     maxRange: 3.5,
+//     link: "https://www.google.com",
+//     note: "Important Note about the Role and Skillset if any",
+//   },
+// ];
 
 // DATA ANALYST related Jobs
 const dataAnalyst = [
   {
-    headline: "DATA ANALYST RELATED JOBS",
+    headline: "Data Analyst - Intern",
     mode: "F2F",
     date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
+    experience: "0 - 1 yrs",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: 5,
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "Exposure to different Software & Applications Work from Home & Flexible working hours Interns get an IAC Associate designation with Cloud Counselage Pvt. Ltd. to get continuous Industry Exposure & Experience till their graduation. This designation can be showcased in their CVs Opportunity to become Internship lead for the future internship program Opportunity for permanent employment after graduation",
   },
   {
-    headline: "Job Headline 1",
+    headline: "Hiring for Data Process Analyst",
     mode: "F2F",
     date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
+    experience: "Fresher",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: "Bulk Hiring",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "BULK HIRING . FRESHERS OPENINGS . GRADUATE,UNDERGRADUATES,Diplama holders 2022,23,24 passout with basic computer skills and finance knowledge with good communication . immidiate joiner only",
   },
   {
-    headline: "Job Headline 1",
+    headline: "Hiring for Data Analyst",
     mode: "F2F",
     date: "12th Sep 2024",
-    location: "Pondicherry",
-    minRange: 3.25,
-    maxRange: 3.5,
-    link: "https://www.google.com",
-    note: "Important Note about the Role and Skillset if any",
+    experience: "Fresher's only",
+    location: "Chennai",
+    salary: "25k - 30k",
+    nVacancies: "Bulk Hiring",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "Basic  in Business Intelligence & Data Visualization. Basic  of experience VBA and Excel. Basic technical knowledge about best practices in VBA automation.Understanding of data modelling techniques, meta data preparation, types of mapping etc. would be a plus.nowledge about Azure platform is a benefit.Knowledge about finance/ controlling also considered a benefit understanding of basic concepts)Ability to work with colleagues from diverse geographies.Advance Excel killsSelf-motivated, must be able to work independently, prioritize daily activities.Flexible to work in ny business shift as per the business requirement.",
   },
 ];
 
-let fullListOfJobs = [internships, wfhJobs, bpoJobs, dataAnalyst];
+// DESIGNING related Jobs
+const designingJobs = [
+  {
+    headline: "Java Developer",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    experience: "1 - 3 yrs",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: "3",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "We are seeking a talented and detail-oriented Java Developer to join our development team. The ideal candidate will have a strong background in Java programming, software development practices, and working in an Agile environment. As a Java Developer, you will be responsible for designing, implementing, and maintaining scalable and efficient Java-based applications.",
+  },
+  {
+    headline: "Full Stack Developer",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    experience: "1 - 3 yrs",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: "3",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "We are looking for a Full Stack Developer to produce scalable software solutions. As a Full Stack Developer, you should be comfortable around both front-end and back-end coding languages, development frameworks and third-party libraries. Proficiency in ReactJS, NodeJS. Familarity in HTML CSS JS JQUERY and Bootstrap",
+    requirement: "REACTJS NODEJS jQuery Bootstrap C / C++ / Java / Python",
+  },
+];
+
+// DIGITAL MARKETING related Jobs
+const digitalMarkJobs = [
+  {
+    headline: "Digital Marketing - Intern",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: 5,
+    experience: "0 - 1 yrs",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "Digital marketing executive who has more knowledge in SEO , SMO , SEM , managing the position of the websites in search engines , running campaigns , Maintaining eCommerce portal and everything compared to the digital world. He / She should have the skill to work with organic searches , competitor analysis , Google analytical tool , webmaster tool , social media channels and more",
+  },
+  {
+    headline: "Social Media Marketer",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    location: "Chennai",
+    salary: "25k - 35k",
+    nVacancies: 5,
+    experience: "0 - 1 yrs",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "We are looking for candidates who were completed Bachelors / Diploma in Marketing / Visual Communication or having any other relevant certified courses. Having experience in handling social media post and SEO related works will be preferrable.",
+  },
+  {
+    headline: "Content Writer",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    location: "Coimbatore",
+    salary: "Not Disclosed",
+    nVacancies: 5,
+    experience: "0 - 6 yrs",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "The ideal candidate will have a strong command of the English language, excellent writing skills, and the ability to produce engaging, high-quality content across various platforms. As a Content Writer, you will be responsible for creating compelling articles, blog posts, website copy, and other marketing materials to attract and engage our target audience.",
+  },
+  {
+    headline: "SEO Analyst",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    location: "Coimbatore",
+    salary: "Not Disclosed",
+    nVacancies: 5,
+    experience: "0 - 3 yrs",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "Candidate will be responsible for implementing effective SEO strategies to improve the online visibility of our website and increase organic search traffic. As an SEO Specialist, you will work closely with content creators, developers, and marketing teams",
+  },
+  {
+    headline: "AR CALLER",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    location: "Coimbatore",
+    salary: "Not Disclosed",
+    nVacancies: 5,
+    experience: "1 - 2 yrs",
+    typeOfJob: "Full Time",
+    link: "https://wa.me/message/5JOBCUDVYJ5EA1",
+    note: "The AR Caller will be responsible for contacting customers to follow up on outstanding invoices, ensuring timely payments, and managing accounts to maintain a healthy cash flow for the company. The ideal candidate will have excellent communication skills and attention to detail.",
+  },
+];
+
+const softwareTesting = [
+  {
+    headline: "Associate Software Test Automation Engineer",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: 1,
+    experience: "2 - 5 yrs",
+    typeOfJob: "Full Time",
+    link: "https://www.naukri.com/job-listings-associate-software-test-automation-engineer-wolters-kluwer-medknow-publications-chennai-2-to-5-years-131124502455?src=seo_srp&sid=17314979362709078&xp=1&px=1",
+    note: "As a QE engineer, you will work in a product development team that develops highly configurable software in a collaborative environment employing agile development methodologies. As an individual contributor, you participate in the entire life cycle of product development and contribute to build quality product offerings",
+    requirment: "Automation-Testing Devops agile apiTesting",
+  },
+  {
+    headline: "Senior Software Engineer Testing",
+    mode: "F2F",
+    date: "12th Sep 2024",
+    location: "Chennai",
+    salary: "Not Disclosed",
+    nVacancies: 1,
+    experience: "3 - 4 yrs",
+    typeOfJob: "Full Time",
+    link: "https://www.naukri.com/job-listings-senior-software-engineer-in-test-simplisafe-kolkata-mumbai-new-delhi-hyderabad-pune-chennai-bengaluru-3-to-4-years-131124502979?src=seo_srp&sid=17314979362709078&xp=2&px=1",
+    note: "we’ve created a culture here that cares just as deeply about the career you’re building. Ours is a no ego culture of collaboration and innovation where those seeking their next challenge can find ig opportunities and make a huge impact on the lives of all those who we protect We don’t just want you to work here We want you to grow and thrive here",
+    requirment: "Python GitHub Software Development Selenium SCRUM agile",
+  },
+];
+
+// let fullListOfJobs = [
+//   internships,
+//   wfhJobs,
+//   bpoJobs,
+//   dataAnalyst,
+//   designingJobs,
+//   digitalMarkJobs,
+// ];
+
+document.querySelector(".jobs-list").innerHTML = internships.map(
+  (internship) => `<div class="job">
+      <div class="job-main">
+      <h4 class="job-headline">${internship.headline}</h4>
+  <div class="important-info">
+  <p class="location">Location: ${internship.location}</p>
+  <p class="date">Vacancies: ${internship.nVacancies}</p>
+  <p class="date">Experience: ${internship.experience}</p>
+  <p class="mode">Job Type: ${internship.typeOfJob}</p>
+  <p class="package">Salary: ${internship.salary}</p>
+  </div>
+  <div class="job-short-description">
+  ${internship.note}
+  <p class="package">${
+    internship.requirement
+      ? "<strong>Requirements:</strong> " + internship.requirement
+      : ""
+  }</p>
+  </div>
+  </div>
+  <div class="apply-link">
+  <a href="${
+    internships.link
+  }" target="_blank" class="apply-link">APPLY LINK</a>
+  </div>
+  </div>`
+);
 
 // Categories Toggler
 const cateJobs = document.querySelectorAll(".category");
@@ -162,9 +291,13 @@ for (let index = 0; index < cateJobs.length; index++) {
     } else if (index == 1) {
       jobs = wfhJobs;
     } else if (index == 2) {
-      jobs = bpoJobs;
-    } else if (index == 3) {
       jobs = dataAnalyst;
+    } else if (index == 3) {
+      jobs = digitalMarkJobs;
+    } else if (index == 4) {
+      jobs = designingJobs;
+    } else if (index == 5) {
+      jobs = softwareTesting;
     }
     let currentCate = "";
     jobs.map((job) => {
@@ -173,13 +306,17 @@ for (let index = 0; index < cateJobs.length; index++) {
       <div class="job-main">
       <h4 class="job-headline">${job.headline}</h4>
   <div class="important-info">
-  <p class="location">${job.location}</p>
-  <p class="date">${job.date}</p>
-  <p class="mode">${job.mode}</p>
-  <p class="package">Rs.${job.minRange} - ${job.maxRange} LPA</p>
+  <p class="location">Location: ${job.location}</p>
+  <p class="date">Vacancies: ${job.nVacancies}</p>
+  <p class="date">Experience: ${job.experience}</p>
+  <p class="mode">Job Type: ${job.typeOfJob}</p>
+  <p class="package">Salary: ${job.salary}</p>
   </div>
   <div class="job-short-description">
   ${job.note}
+  <p class="package">${
+    job.requirement ? "<strong>Requirements:</strong> " + job.requirement : ""
+  }</p>
   </div>
   </div>
   <div class="apply-link">
